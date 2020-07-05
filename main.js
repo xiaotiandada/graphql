f04132e4341f99fa6c54a244dc7127d958c1e75b
xiaotian <952822399@qq.com>
2020-07-05 23:04:45 +0800

    graphql

--- main.js
+++ main.js
@@ -2,15 +2,119 @@ var express = require('express');
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
 
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides;
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+
+  roll({numRolls}) {
+    var output = [];
+    for (var i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce());
+    }
+    return output;
+  }
+}
+
+// 如果 Message 拥有复杂字段，我们把它们放在这个对象里面。
+class Message {
+  constructor(id, {content, author}) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+}
+
 var schema = buildSchema(`
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
+  }
+  
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
   type Query {
     hello: String,
     rollDice(numDice: Int!, numSides: Int): [Int],
     quoteOfTheDay: String,
     random: Float!,
-    rollThreeDice: [Int]
+    rollThreeDice: [Int],
+    getDie(numSides: Int): RandomDie,
+    getMessage(id: ID!): Message,
+    ip: String,
+    user(id: String): User
+  },
+  type User {
+    id: String
+    name: String
   }
 `);
+
+const loggingMiddleware = (req, res, next) => {
+  console.log('ip:', req.ip);
+  next();
+}
+
+// 从 id 映射到 User 对象
+var fakeDatabase = {
+  'a': {
+    id: 'a',
+    name: 'alice',
+  },
+  'b': {
+    id: 'b',
+    name: 'bob',
+  },
+};
+
+var fakeDatabase = {};
+
+
+// 定义 User 类型
+var userType = new graphql.GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: graphql.GraphQLString },
+    name: { type: graphql.GraphQLString },
+  }
+});
+
+// 定义 Query 类型
+var queryType = new graphql.GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    user: {
+      type: userType,
+      // `args` 描述了 `user` 查询接受的参数
+      args: {
+        id: { type: graphql.GraphQLString }
+      },
+      resolve: (_, {id}) => {
+        return fakeDatabase[id];
+      }
+    }
+  }
+});
+
+var schema = new graphql.GraphQLSchema({query: queryType});
 
 var root = { 
   hello: () => 'Hello world!',
@@ -20,10 +124,48 @@ var root = {
   random: () => Math.random(),
   rollThreeDice: () => {
     return [1,2,3].map(_ => Math.floor(Math.random() * 6) + 1)
+  },
+  rollDice: ({numDice, numSides}) => {
+    var output = [];
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
+  },
+  getDie: ({numSides}) => {
+    return new RandomDie(numSides || 6);
+  },
+  getMessage: ({id}) => {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    return new Message(id, fakeDatabase[id]);
+  },
+  createMessage: ({input}) => {
+    // Create a random id for our "database".
+    var id = require('crypto').randomBytes(10).toString('hex');
+
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+  updateMessage: ({id, input}) => {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    // This replaces all old data, but some apps might want partial update.
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+  ip: function (args, request) {
+    return request.ip;
+  },
+  user: ({id}) => {
+    return fakeDatabase[id];
   }
 };
 
 var app = express();
+app.use(loggingMiddleware);
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
