8d5ffdb2f34b67ac2e5415383a007d1c530d000c
xiaotian <952822399@qq.com>
2020-07-07 12:59:21 +0800

    feat: 初体验

--- app.js
+++ app.js
@@ -4,52 +4,74 @@ const { ApolloServer, gql } = require('apollo-server-express');
 // 书库 没接数据库
 const books = [
   {
+    id: 0,
     title: 'Harry Potter and the Chamber of Secrets',
     author: 'J.K. Rowling',
   },
   {
+    id: 1,
     title: 'Jurassic Park',
     author: 'Michael Crichton'
   },
 ];
 
-// TODO: 
-// 1. 查询所有的书
-// 2. 添加一本书
-// 3. 更新一本书
-// 4. 删除一本书
-
-
 const typeDefs = gql`
   type Book {
+    id: Int,
     title: String
-    author: Author
+    author: String
   }
-  type Author {
-    name: String
-    books: [Book]
-  }
-
 
   type Query {
-    hello: String,
     books: [Book],
-    authors: [Author]
   }
 
   type Mutation {
-    addBook(title: String, author: String): Book
+    createBook(title: String, author: String): Book,
+    updateBook(id: Int, title: String, author: String): Book,
+    deleteBook(id: Int, title: String, author: String): Book,
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
     books: () => books,
-    authors: () => books,
   },
   Mutation: {
-    
+    // 创建
+    createBook: (_, { title, author }) => {
+      let list = {
+        id: books.length,
+        title,
+        author
+      }
+      books.push(list)
+      return list
+    },
+    // 更新
+    updateBook: (_, { id, title, author }) => {
+      let idx = books.findIndex(i => i.id === id)
+      if (~idx) {
+        books[idx] = {
+          id: idx,
+          title: title,
+          author: author
+        }
+        return books[idx]
+      } else {
+        return []
+      }
+    },
+    // 删除
+    deleteBook: (_, { id }) => {
+      let idx = books.findIndex(i => i.id === id)
+      if (~idx) {
+        books.splice(idx, 1)
+        return books[idx]
+      } else {
+        return []
+      }
+    }
   }
 };
 
@@ -61,3 +83,24 @@ server.applyMiddleware({ app });
 app.listen({ port: 4000 }, () =>
   console.log('Now browse to http://localhost:4000' + server.graphqlPath)
 );
+
+
+// query GetBooks {
+//   books {
+//     id,
+//     title
+//     author
+//   }
+// }
+
+// mutation Create {
+//   createBook(title: "12312", author: "xxxxx") {title, author},
+// }
+
+// mutation Update {
+//   updateBook(id: 0, title: "123", author: "xxxxx") {id, title, author}
+// }
+
+// mutation Delete {
+//   deleteBook(id: 1) {id, title, author}
+// }
